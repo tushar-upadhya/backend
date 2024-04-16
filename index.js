295de36181d27eb70a54e2409467eb9256dc8cbf
tushar-upadhya <tusharupadhyay691@gmail.com>
2024-04-16 11:47:16 +0530

    .env

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("a");
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+const PORT = 4000;
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.get("/login", (req, res) => {
+  res.send("<h1>login Page!</h1>");
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("PORT:", PORT);
+});
